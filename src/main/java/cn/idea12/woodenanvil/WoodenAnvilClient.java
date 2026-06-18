af71026d332cd319707dc54abfda04145f491e5e
Steverjava <steverjava@outlook.com>
2026-06-18 22:11:48 +0800

    拆分类

--- src/main/java/cn/idea12/woodenanvil/WoodenAnvilClient.java
+++ src/main/java/cn/idea12/woodenanvil/WoodenAnvilClient.java
@@ -1,12 +1,11 @@
+// This file is licensed under the MIT License.
+
 package cn.idea12.woodenanvil;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
--- src/main/java/cn/idea12/woodenanvil/WoodenAnvilEvents.java
+++ src/main/java/cn/idea12/woodenanvil/WoodenAnvilEvents.java
@@ -1,3 +1,5 @@
+// This file is licensed under the MIT License.
+
 package cn.idea12.woodenanvil;
 
 import net.minecraft.world.item.BlockItem;
